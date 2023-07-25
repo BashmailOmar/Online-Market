468f4e9e393931905aac632445149dba2bd954a4
BashmailOmar <bashmailomar@gmail.com>
2023-07-25 13:48:24 +0300

    updating product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './products/components/all-products/all-products.component';
+import { ProductsDetailsComponent } from './products/components/products-details/products-details.component';
+import { CartComponent } from './carts/components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "products", component: AllProductsComponent },
+  { path: "details", component: ProductsDetailsComponent },
+  { path: "cart", component: CartComponent },
+  { path: "**", redirectTo: "products", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
