fe585910f515bc3a468aba811a4f6cb83ab9a09b
Omar <bashmailomar@gmail.com>
2023-07-26 05:39:08 +0300

    finish 03

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { SelectComponent } from './component/select/select.component';
 import { HeaderComponent } from './component/header/header.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -10,9 +11,11 @@ import { HeaderComponent } from './component/header/header.component';
   declarations: [
     NavbarComponent,
     SelectComponent,
+
     HeaderComponent
   ],
   imports: [
+    RouterModule,
     CommonModule
   ],
   exports: [
